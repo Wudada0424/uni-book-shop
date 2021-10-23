5d00d19a8f48d06bd6c8ef226774b8e89db9579c
bysq <15098226936@163.com>
2021-10-24 02:21:44 +0800

    封装api/拦截器

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
 	<view class="content">
-		首页
+		<u-swiper :list="imgList"></u-swiper>
+		<u-tabs :list="sortList" :is-scroll="false"  @change="change"></u-tabs>
 	</view>
 </template>
 
@@ -8,13 +9,75 @@
 	export default {
 		data() {
 			return {
+				imgList: [
+					'https://cdn.uviewui.com/uview/swiper/3.jpg',
+					'https://cdn.uviewui.com/uview/swiper/2.jpg'
+				],
+				sortList: [{
+					name: '默认'
+				}, {
+					name: '销量'
+				}, {
+					name: '推荐',
+				},{
+					name : '最新商品'
+				}]
 			}
 		},
-		onLoad() {
+		async onLoad() {
+			// this.$u.get('/api/index').then(res => {
+			// console.log(res)
+			// })
+			// console.log(21213123)
 
+			// await 必须写在async 函数中
+			// const res = await this.$u.get('/api/index',)
+			// console.log(res)
+			// console.log(21213123)
+
+
+			// const res = await this.$u.get('/api/index', )
+			// console.log(res) 
+			// const res = await this.$u.post('/api/auth/login',)
+			// console.log(res)
+
+			// try {
+			// 	const res = await this.$u.post('/api/auth/login',)
+			// } catch (e) {
+
+			// }
+
+
+			// const res = await this.$u.patch('/api/orders/1/confirm',{name : 'Tom'});
+			// console.log(res)
+
+
+			//集中管理api
+			// const res = await this.$u.api.index();
+			// console.log(res)
+
+			//测试登录
+			// const data = {
+			// 	email : 'test@a.com',
+			// 	password : '123123'
+			// }
+			// const res = await this.$u.api.authLogin(data )
+			// console.log(res)
+
+			// // const ress = await this.$u.api.aaa()
+			// // console.log(ress)
+
+			// //测试 vuex
+			// console.log(this.vuex_version)
+
+			// // this.$u.vuex('vuex_name' , 'Tom')
+
+			// console.log(this.vuex_name)
 		},
 		methods: {
-
+			change(index) {
+				this.current = index;
+			}
 		}
 	}
 </script>
